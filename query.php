e1450985909a948e5412fea8e0e428aa76a81144
Brian Bolnick <brianbolnick@gmail.com>
2018-02-05 00:04:30 -0700

    update query file

--- query.php
+++ query.php
@@ -1,12 +1,12 @@
 <?php 
-    require_once 'login.php';
+    require_once 'db_config.php';
     $conn = new mysqli($hn, $un, $pw, $db);
     echo $hn;
     echo $un;
     echo $pw;
     echo $db;
     if ($conn->connect_error) die($conn->connect_error);
-    // $query = "DROP TABLE `payment`;";
+    $query = "CREATE DATABASE `sellout`;";
     // $query = "CREATE TABLE `order_line` ( `order_id` INT NOT NULL, `product_id` INT NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
     // $query = "ALTER TABLE `order_line` ADD KEY `product_id` (`product_id`), ADD KEY `order_id` (`order_id`);";
     // $query = "CREATE TABLE `payment` (`payment_id` INT NOT NULL,`cc_number` varchar(32) NOT NULL DEFAULT '',`exp_date` datetime DEFAULT NULL,`cvc` varchar(4) NOT NULL DEFAULT '', PRIMARY KEY AUTOINCREMENT (`payment_id`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;";
