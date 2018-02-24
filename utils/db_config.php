6e8894a8b5b411d8ca9bb2a6db00717b73cf1bcd
Brian Bolnick <brianbolnick@gmail.com>
2018-02-24 08:37:03 -0700

    update db

--- utils/db_config.php
+++ utils/db_config.php
@@ -1,13 +1,13 @@
 <?php
-    $hn = "localhost"; //host name
-    $db = "sellout"; //database
-    $un = "root"; //username
-    $pw = "root"; //password -- on Mac OS it is also root
+    // $hn = "localhost"; //host name
+    // $db = "sellout"; //database
+    // $un = "root"; //username
+    // $pw = "root"; //password -- on Mac OS it is also root
 
 
     // for production only - yeah... I know I shouldn't have passwords here..
-    // $hn = "us-cdbr-iron-east-05.cleardb.net";
-    // $un = "b0eb2e2a53e1e5";
-    // $pw = "f6a5f377";
-    // $db = "heroku_d3a907f83e85ea4";
+    $hn = "us-cdbr-iron-east-05.cleardb.net";
+    $un = "b0eb2e2a53e1e5";
+    $pw = "f6a5f377";
+    $db = "heroku_d3a907f83e85ea4";
 ?>
