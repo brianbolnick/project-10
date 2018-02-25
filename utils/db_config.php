db0f97b7416caa7feed55409a29f5da80b8623e2
Brian Bolnick <brianbolnick@gmail.com>
2018-02-25 02:35:10 -0700

    update db config file to work in both prod and local

--- utils/db_config.php
+++ utils/db_config.php
@@ -1,13 +1,14 @@
 <?php
-    $hn = "localhost"; //host name
-    $db = "sellout"; //database
-    $un = "root"; //username
-    $pw = "root"; //password -- on Mac OS it is also root
 
-
-    // for production only - yeah... I know I shouldn't have passwords here..
-    // $hn = "us-cdbr-iron-east-05.cleardb.net";
-    // $un = "b0eb2e2a53e1e5";
-    // $pw = "f6a5f377";
-    // $db = "heroku_d3a907f83e85ea4";
+if (isset($_ENV['DB_HN'])) {
+    $hn = $_ENV['DB_HN'];
+    $un = $_ENV['DB_UN'];
+    $pw = $_ENV['DB_PW'];
+    $db = $_ENV['DB_DB'];
+} else {
+    $hn = "localhost";
+    $db = "sellout";
+    $un = "root";
+    $pw = "root";
+}
 ?>
