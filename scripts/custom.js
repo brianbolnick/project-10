b3d4d8f2d9d16afc4a1e512bc518b9629e6c4459
Brian Bolnick <brianbolnick@gmail.com>
2018-03-01 18:54:42 -0700

    add secret JS....shhhhh

---
+++ scripts/custom.js
@@ -0,0 +1,5 @@
+$(function () {
+    $('div#secret-secret').click(function () {
+        window.location.replace("https://www.youtube.com/watch?v=U9t-slLl30E");
+    });
+});
--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,10 @@
 <footer>
     <nav class="navbar fixed-bottom navbar-light bg-light" style="background: transparent !important; justify-content: flex-end">
         <ul class="navbar-nav" >
+            <li>
+                <div class="secret-secret" id='secret-secret'>
+                </div>
+            </li>
             <li class="nav-item">
                 <a class="nav-link footer-link" href="#">&copy; 2018</a>
             </li>
